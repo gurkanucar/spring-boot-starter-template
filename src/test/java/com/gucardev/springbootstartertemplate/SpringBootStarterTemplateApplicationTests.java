e8aee27f92d5f8179c7bfc7538b7e095468cf8a8
Gurkan UCAR <ucar.gurkan@hotmail.com>
2025-02-24 17:25:08 +0300

    project initialized

---
+++ src/test/java/com/gucardev/springbootstartertemplate/SpringBootStarterTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootstartertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
