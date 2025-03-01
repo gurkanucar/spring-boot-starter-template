a96a2bfa9228dde23449c95774e396afc731a56f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2025-03-01 17:20:00 +0300

    prometheus config updated

--- src/main/java/com/gucardev/springbootstartertemplate/infrastructure/config/prometheus/PrometheusConfiguration.java
+++ src/main/java/com/gucardev/springbootstartertemplate/infrastructure/config/prometheus/PrometheusConfiguration.java
@@ -1,6 +1,7 @@
 package com.gucardev.springbootstartertemplate.infrastructure.config.prometheus;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PrometheusConfiguration {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        return registry -> registry.config().commonTags("application", "my-project-name");
+        return registry -> registry.config().commonTags("application", applicationName);
     }
 
 }
