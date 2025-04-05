7ec0e5df7378f528545d4345c12327f2d9c3ad89
Gurkan UCAR <ucar.gurkan@hotmail.com>
2025-04-05 18:09:35 +0300

    StarterController updated

--- src/main/java/com/gucardev/springbootstartertemplate/infrastructure/config/starter/StarterController.java
+++ src/main/java/com/gucardev/springbootstartertemplate/infrastructure/config/starter/StarterController.java
@@ -1,15 +1,15 @@
 package com.gucardev.springbootstartertemplate.infrastructure.config.starter;
 
 
-import com.gucardev.springbootstartertemplate.infrastructure.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -27,14 +27,8 @@ public class StarterController {
     }
 
     @GetMapping({"/timezone", "/time"})
-    public ResponseEntity<SuccessResponse<Object>> getTimezone() {
-        return ResponseEntity.ok(
-                SuccessResponse.builder()
-                        .status(HttpStatus.OK)
-                        .messageKey("response.user.created")
-                        .data(timeZoneHelper.getCurrentTimeZoneInfo())
-                        .build()
-        );
+    public Map<String, String> getTimezone() {
+        return timeZoneHelper.getCurrentTimeZoneInfo();
     }
 
     @PutMapping("/log-level")
